4fe2e7c229dc9e479775d2bf6c5a7b9761c9758f
Higor <higor.fidelis.2@gmail.com>
2026-04-27 21:46:35 -0300

    aaaaaaa

--- test/test.py
+++ test/test.py
@@ -1,10 +1,12 @@
 
+import pytest
 from src.main import *
 
-def test_soma(a, b):
-    assert soma(2,3) == 5
+def test_soma():
+    assert soma(2, 3) == 5
 
 
-def test_funcionamento_soma():
-    resultado = funcionamento_soma(2, 3)
+@pytest.mark.asyncio
+async def test_funcionamento_soma():
+    resultado = await funcionamento_soma(2, 3)
     assert resultado["resultados"] == 5
